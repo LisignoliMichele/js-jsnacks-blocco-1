437f75016e692310be6b6944029defe6437c716d
Michele Lisignoli <michele@lisignoli.com>
2021-03-02 12:26:03 +0100

    snack 4

---
+++ snack 4/js/script.js
@@ -0,0 +1,16 @@
+// In un array sono contenuti i nomi degli invitati alla festa del grande Inzaghi,
+var invitedList = ["Marco", "Alex", "Gianluca", "Paola", "Arianna", "Giovanni", "Samuele" ]
+// chiedi all’utente il suo nome
+var userName = prompt("What's your name?")
+var invited = false;
+// comunica all'utente se può partecipare o no alla festa.
+for (var i = 0; i < invitedList.length; i++){
+  if (userName == invitedList) {
+    invited = true;
+  }
+}
+if (invited = true){
+  alert("You are in, let's party")
+} else{
+  alert("Sorry, your name it's not in the list, bye bye")
+}
