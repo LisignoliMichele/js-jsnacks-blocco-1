d799e67e029f337392766b6ea63202921b73f2b4
Michele Lisignoli <michele@lisignoli.com>
2021-03-02 12:15:45 +0100

    snack 3

--- snack_3/js/script.js
+++ snack_3/js/script.js
@@ -1,14 +1,12 @@
 // Il software deve chiedere per 10 volte all’utente di inserire un numero.
-var numberOne = parseInt(prompt("enter a number"));
-var numberTwo = parseInt(prompt("enter another number"));
-var numberThree = parseInt(prompt("enter another number"));
-var numberFor = parseInt(prompt("enter another number"));
-var numberFive = parseInt(prompt("enter another number"));
-var numberSix = parseInt(prompt("enter another number"));
-var numberSeven = parseInt(prompt("enter another number"));
-var numberHeight = parseInt(prompt("enter another number"));
-var numberNine = parseInt(prompt("enter another number"));
-var numberTen = parseInt(prompt("enter another number"));
+var sum = 0;
 // Il programma stampa la somma di tutti i numeri inseriti.
-var sum = numberOne + numberTwo + numberThree + numberFor + numberFive + numberSix + numberSeven + numberHeight + numberNine + numberTen;
-alert("the amount of the sum of all your numbers is" + sum)
+
+for (var i = 1; i <= 10; i++){
+  var number = parseInt(prompt("Please anter a number"));
+  if ( !isNaN(number) ){
+    sum = sum + number;
+  }
+}
+
+alert("the ammount of all your numbers is: " + sum)
