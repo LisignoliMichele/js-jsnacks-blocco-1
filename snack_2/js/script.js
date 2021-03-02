fed599c206bdf7479a891601b89e3b7b9e00cbe6
Michele Lisignoli <michele@lisignoli.com>
2021-03-02 11:48:06 +0100

    snack 2

--- snack_2/js/script.js
+++ snack_2/js/script.js
@@ -5,9 +5,9 @@ var secondWord = prompt("Enter another word");
 // Il software stampa prima la parola più corta, poi la parola più lunga
 
 if (firstWord.length > secondWord.length) {
-  alert(' The first word " ' + firstWord + '" it\'s the longer one and the second one "' + secondWord + '"its the shorter' );
+  alert(' The first word " ' + firstWord + '" it\'s the longer one and the second one "' + secondWord + '" its the shorter' );
 } else if (firstWord.length < secondWord.length) {
-  alertalert(' The second word " ' + secondWord + '" it\'s the longer one and the first one "' + firstWord + '"its the shorter' );
+  alert(' The second word " ' + secondWord + '" it\'s the longer one and the first one "' + firstWord + '" its the shorter' );
 }else {
   alert(secondWord + " " + firstWord + " The words are long same" );
 }
