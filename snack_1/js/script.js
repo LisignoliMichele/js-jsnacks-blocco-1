08a5f1cb53364c0c8f1a6d55313dff5f21674523
Michele Lisignoli <michele@lisignoli.com>
2021-03-02 11:17:26 +0100

    snack 1

---
+++ snack_1/js/script.js
@@ -0,0 +1,11 @@
+// L’utente inserisce due numeri in successione, con due prompt.
+var numberOne = prompt("enter a number");
+var numberTwo = prompt("enter another number");
+// Il software stampa il maggiore.
+if (numberOne > numberTwo){
+  alert('the first number"' + numberOne ''" it\'s the bigger one')
+} else if (numberTwo > numberOne){
+  alert('the second number"' + numberTwo ''" it\'s the bigger one')
+}else{
+  alert("The numbers are the same")
+}
