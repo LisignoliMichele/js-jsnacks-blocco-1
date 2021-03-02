63e513cb1561c25f8c0bddb044a5a785f908385d
Michele Lisignoli <michele@lisignoli.com>
2021-03-02 11:46:37 +0100

    snack 2

---
+++ snack_2/js/script.js
@@ -0,0 +1,13 @@
+// L’utente inserisce due parole in successione, con due
+// prompt.
+var firstWord = prompt("Enter a word");
+var secondWord = prompt("Enter another word");
+// Il software stampa prima la parola più corta, poi la parola più lunga
+
+if (firstWord.length > secondWord.length) {
+  alert(' The first word " ' + firstWord + '" it\'s the longer one and the second one "' + secondWord + '"its the shorter' );
+} else if (firstWord.length < secondWord.length) {
+  alertalert(' The second word " ' + secondWord + '" it\'s the longer one and the first one "' + firstWord + '"its the shorter' );
+}else {
+  alert(secondWord + " " + firstWord + " The words are long same" );
+}
