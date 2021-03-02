098e2958a2a1dfdf5f43de89cf1a82791358ffcc
Michele Lisignoli <michele@lisignoli.com>
2021-03-02 13:09:35 +0100

    number !isNaN

---
+++ snack_5/js/script.js
@@ -0,0 +1,11 @@
+// Crea un array vuoto.
+var oddNumbers = [];
+// Chiedi per 6 volte all’utente di inserire un numero
+for (var i = 1; i <= 6; i++){
+  var number = parseInt(prompt("Please anter a number"));
+  // se è dispari inseriscilo nell’array.
+  if (!isNaN(number) && number % 2 == 1 ){
+    oddNumbers +=  number;
+  }
+}
+console.log(oddNumbers)
