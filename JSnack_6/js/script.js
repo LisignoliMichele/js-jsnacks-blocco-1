1fe47578240220d14f284943c63626882c9163a4
Michele Lisignoli <michele@lisignoli.com>
2021-03-03 12:12:35 +0100

    JSnack 6

--- JSnack_6/js/script.js
+++ JSnack_6/js/script.js
@@ -1,5 +1,12 @@
 // Stampa il cubo dei primi N numeri, dove N è un numero indicato dall’utente.
-var userNumber = parseInt(prompt("Enter one number"));
+// ask the userNumber
+do {
+  var userNumber = parseInt(prompt("Enter one number"));
+} while ( isNaN (userNumber));
+// print cubes
+
 for (var i = 1; i <= userNumber; i++){
-  console.log( i * i * i )
+  // cube = i x i x i
+  var cube = Math.pow(i, 3);
+  console.log( cube )
 }
